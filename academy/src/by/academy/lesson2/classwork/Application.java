5828448c48799ab872b9ecb81a02e75daeab0056
Elena Lakotka <lakotkaelena8@gmail.com>
2022-11-22 20:33:53 +0300

    test

---
+++ academy/src/by/academy/lesson2/classwork/Application.java
@@ -0,0 +1,20 @@
+package by.academy.lesson2.classwork;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Cat cat1 = new Cat();
+		cat1.grow();
+		cat1.grow();
+		cat1.grow();
+		System.out.println(cat1.age);
+		System.out.println(cat1.age);
+		System.out.println(cat1.age);
+
+		Cat cat2 = new Cat("Tom");
+		cat2.eat();
+		cat2.sleep();
+		cat2.walk();
+	}
+
+}
---
+++ academy/src/by/academy/lesson2/classwork/Cat.java
@@ -0,0 +1,29 @@
+package by.academy.lesson2.classwork;
+
+public class Cat {
+	int age = 8;
+	String nickname = "Tom";
+
+	public void grow() {
+		age++;
+	}
+
+	public void sleep() {
+		System.out.println("Кот спит");
+	}
+
+	public void eat() {
+		System.out.println("Кот ест");
+	}
+
+	public void walk() {
+		System.out.println("Кот гуляет");
+	}
+
+	public Cat() {
+	}
+
+	public Cat(String nickname) {
+		this.nickname = nickname;
+	}
+}
